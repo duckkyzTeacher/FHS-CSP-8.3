b50b92d66380d3887c0e5c40f281e15fc39976c0
Benjamin Johnson <bjohnson@fcusd.org>
2024-10-14 21:25:29 -0700

    Update and rename Program7-1.py to Program8-3.py

---
+++ Program8-3.py
@@ -0,0 +1,31 @@
+## Problem 1 ##
+#Please write a script that: 
+# - prints a 5x5 grid of * symbols.
+
+
+
+## Problem 2 ##
+#Please write a script that: 
+# - prints a right-angled triangle of *.
+
+
+
+
+## Problem 3 ##
+#Please write a script that: 
+# - prints the following pattern using nested loops:
+#1
+#12
+#123
+#1234
+#12345
+
+
+
+
+## Problem 4 ##
+#Please write a script that: 
+# - Creates a multiplication table from 1 to 10.
+
+
+
